b35d8f4db6a93b169a720ecdc0d62813a28a0d83
Stas_Che <86572188+Chetverukhin@users.noreply.github.com>
2023-08-20 11:18:33 +0400

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AuthenticationComponent } from './authentication/authentication.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    AuthenticationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/authentication/authentication.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-authentication',
+  templateUrl: './authentication.component.html',
+  styleUrls: ['./authentication.component.css']
+})
+export class AuthenticationComponent {
+
+  authenticationForm: FormGroup
+
+  constructor(private _builder: FormBuilder)
+  {
+    this.authenticationForm = _builder.group({
+      'emailInput': [''],
+      'passwordInput' : ['']
+    })
+  }
+
+  login(){
+    console.log(this.authenticationForm.value)
+  }
+
+
+
+}
