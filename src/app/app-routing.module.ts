5092f7be279c726f334a62bcd85cfb47f8d64848
Stas_Che <86572188+Chetverukhin@users.noreply.github.com>
2023-08-22 00:03:26 +0400

    add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'logon', component: RegistrationComponent},
+  {path: 'login', component: AuthenticationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
